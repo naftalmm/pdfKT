f3c305f1b9a46c60f2a28e22a3848d49c3c2c1b6
naftalmm <naftalmm@gmail.com>
2020-07-18 20:38:19 +0300

    add scrollbar to images previews

--- src/main/kotlin/JPDFDocumentEditView.kt
+++ src/main/kotlin/JPDFDocumentEditView.kt
@@ -1,22 +1,22 @@
 
-import java.awt.BorderLayout
+import java.awt.Dimension
 import java.awt.FlowLayout
 import java.awt.Frame
-import javax.swing.JDialog
-import javax.swing.JPanel
+import javax.swing.*
 
 class JPDFDocumentEditView(owner: Frame, pdf: PDFDocument) : JDialog(owner, pdf.fileName) {
     init {
-        layout = FlowLayout()
+        layout = BoxLayout(contentPane, BoxLayout.Y_AXIS)
+        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT)
+        defaultCloseOperation = DISPOSE_ON_CLOSE
 
-        add(JImage(pdf.currentTitleImage.fit(800)), BorderLayout.CENTER)
+        add(JImage(pdf.currentTitleImage.fit(800)))
+        add(Box.createRigidArea(Dimension(0, 5)))
         val p = JPanel(FlowLayout())
         for ((pageIndex, rotation) in pdf.getCurrentState().pages) {
             val image = JImage(pdf.imagesThumbnails[pageIndex])
             p.add(if (rotation != Rotation.NORTH) image.rotate(rotation.angle.toDouble()) else image)
         }
-        add(p, BorderLayout.SOUTH)
-        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT)
-        defaultCloseOperation = DISPOSE_ON_CLOSE
+        add(JScrollPane(p))
     }
 }
