0d0374f7b2f0e7ce53bc0a1d4d9fe2da438dc07b
naftalmm <naftalmm@gmail.com>
2020-08-06 17:51:30 +0300

    refactoring

--- src/main/kotlin/JPanelCardLayout.kt
+++ src/main/kotlin/JPanelCardLayout.kt
@@ -4,19 +4,14 @@ import java.util.*
 import javax.swing.JPanel
 
 class JPanelCardLayout : JPanel(CardLayout()) {
-    private val componentPseudonyms =
-        WeakHashMap<Component, String>()
+    private val componentPseudonyms = WeakHashMap<Component, String>()
     private var counter = 0
 
-    fun show(comp: Component) {
-        (this.layout as CardLayout).show(this, componentPseudonyms[comp])
-    }
+    fun show(comp: Component) = (this.layout as CardLayout).show(this, componentPseudonyms[comp])
 
-    override fun add(comp: Component): Component {
-        synchronized(this) {
-            val pseudonym = componentPseudonyms.getOrPut(comp) { counter++.toString() }
-            super.add(comp, pseudonym)
-            return comp
-        }
+    override fun add(comp: Component): Component = synchronized(this) {
+        val pseudonym = componentPseudonyms.getOrPut(comp) { counter++.toString() }
+        super.add(comp, pseudonym)
+        return comp
     }
 }
