000f7eda078be5f2e8c92cc4170cc3bfd2daa404
naftalmm <naftalmm@gmail.com>
2020-09-24 23:27:48 +0300

    bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,9 +33,9 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
     implementation("org.icepdf.os:icepdf-core:6.3.0")
-    implementation("com.itextpdf:itext7-core:7.1.3")
+    implementation("com.itextpdf:kernel:7.1.12")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.6.2")
     testImplementation("org.assertj", "assertj-swing-junit", "3.9.2")
     testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
