db5a1d8ef392d763d9b3b4beab4afc667047bba6
naftalmm <naftalmm@gmail.com>
2020-09-25 16:29:53 +0300

    Add shouldSupportDnDRearrangeItems test; minor refactoring

--- src/test/kotlin/GUITests.kt
+++ src/test/kotlin/GUITests.kt
@@ -1,3 +1,4 @@
+import org.assertj.swing.core.ComponentDragAndDrop
 import org.assertj.swing.core.KeyPressInfo.keyCode
 import org.assertj.swing.core.matcher.JButtonMatcher
 import org.assertj.swing.core.matcher.JLabelMatcher.withText
@@ -6,7 +7,8 @@ import org.assertj.swing.edt.GuiActionRunner
 import org.assertj.swing.fixture.FrameFixture
 import org.bouncycastle.util.test.SimpleTest
 import org.junit.jupiter.api.*
-import java.awt.Event.CTRL_MASK
+import java.awt.Point
+import java.awt.event.InputEvent.CTRL_MASK
 import java.awt.event.KeyEvent.VK_O
 import java.io.File
 import java.net.URLDecoder
@@ -15,6 +17,7 @@ import java.util.concurrent.Callable
 
 class PDFKTApplicationTest {
     private lateinit var window: FrameFixture
+    private lateinit var dnd: ComponentDragAndDrop
 
     companion object {
         @JvmStatic
@@ -35,6 +38,9 @@ class PDFKTApplicationTest {
         val frame = GuiActionRunner.execute(Callable { App() })
         window = FrameFixture(frame)
         window.show() // shows the frame to test
+        window.maximize()
+
+        dnd = ComponentDragAndDrop(window.robot())
     }
 
     @Test
@@ -44,8 +50,7 @@ class PDFKTApplicationTest {
         dropPDFsLabel.requireVisible()
 
         //add some PDF
-        window.pressAndReleaseKey(keyCode(VK_O).modifiers(CTRL_MASK))
-            .fileChooser().selectFile(getTestResource("1.pdf")).approve()
+        addPDF("1")
         dropPDFsLabel.requireNotVisible()
 
         //delete added item
@@ -53,8 +58,27 @@ class PDFKTApplicationTest {
         dropPDFsLabel.requireVisible()
     }
 
+    @Test
+    fun shouldSupportDnDRearrangeItems() {
+        addPDF("1")
+        addPDF("2")
+        val pdfsList = window.robot().finder().findByType(JPDFsList::class.java)
+        Assertions.assertEquals("1", pdfsList.getCurrentPDFsState()[0].first.nameWithoutExtension)
+
+        val label2 = window.label(withText("2")).target()
+        dnd.drag(label2, Point(label2.x, label2.y))
+        dnd.drop(label2, Point(0, 0))
+        Assertions.assertEquals("2", pdfsList.getCurrentPDFsState()[0].first.nameWithoutExtension)
+    }
+
+    @Suppress("DEPRECATION")
+    private fun addPDF(name: String) {
+        window.pressAndReleaseKey(keyCode(VK_O).modifiers(CTRL_MASK))
+            .fileChooser().selectFile(getTestResource("$name.pdf")).approve()
+    }
+
     private fun getTestResource(name: String): File =
-        File(URLDecoder.decode(SimpleTest::class.java.getResource("/${name}").file, "UTF-8"))
+        File(URLDecoder.decode(SimpleTest::class.java.getResource("/$name").file, "UTF-8"))
 
     @AfterEach
     fun tearDown() {
